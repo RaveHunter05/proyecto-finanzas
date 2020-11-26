20a13c1db81864bf07e72975ed9d6461df3a1639
RaveHunter05 <paulsotelo97@gmail.com>
2020-11-26 11:18:49 -0600

    noCreado

--- src/selection/Selection.js
+++ src/selection/Selection.js
@@ -2,12 +2,15 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 function Selection() {
+    function noCreado(){
+        alert("este modulo aún no se ha completado :'l")
+    }
     return (
         <div>
             <h2 className="pt-3 pb-3">¿Que desea hacer?</h2>
             <div className="row pt-3">
                 <div className="col">
-                    <button>
+                    <button onClick={noCreado}>
                         Balance general
                     </button>
                 </div>
@@ -19,11 +22,9 @@ function Selection() {
                     </Link>
                 </div>
                 <div className="col">
-                    <Link to="/razones">
-                        <button>
+                        <button onClick={noCreado}>
                             Razones financieras
                         </button>
-                    </Link>
                 </div>
             </div>
         </div>
